7d59ef9bb3b1503b3950e57a8f199c03b424bbb2
marianoan <marianoangonzalez@gmail.com>
2013-10-23 14:29:32 -0300

    4

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 ﻿$(document).ready(function () {
     $(".tablesorter").tablesorter();
-    $("#main").empty();
+    $("#contact_list").show();
+    $("#new_contact_form").hide();
+
+    $(".icn_add_user").on("click", function () {
+        $("#contact_list").hide();
+        $("#new_contact_form").show();
+    });
+
+    $(".icn_view_users").on("click", function () {
+        $("#contact_list").show();
+        $("#new_contact_form").hide();
+    });
+    //$("#main").empty();
 });
 
-$(function () {
-    $('.column').equalHeight();
-});
